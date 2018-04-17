eacededdf81f868ee9c7f34a0769017b6d6f823d
Jorge Rodriguez <jorge.rodriguez@webmaps.com.mx>
2018-04-17 13:16:40 -0500

    Forma1 de crear SnackBar

--- app/src/main/java/mx/com/webmaps/md_ejercicio7/MainActivity.java
+++ app/src/main/java/mx/com/webmaps/md_ejercicio7/MainActivity.java
@@ -1,7 +1,9 @@
 package mx.com.webmaps.md_ejercicio7;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View view = findViewById(R.id.activy_main);
+
+        Snackbar.make(view,"Hello Snackbar",Snackbar.LENGTH_SHORT).show();
     }
 }

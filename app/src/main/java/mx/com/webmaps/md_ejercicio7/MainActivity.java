027c5b3544c70968e49aa9df1b583b5bef443937
Jorge Rodriguez <jorge.rodriguez@webmaps.com.mx>
2018-04-17 13:45:17 -0500

    Mostrar un SnackBar abajo de un FloatingActionButton

--- app/src/main/java/mx/com/webmaps/md_ejercicio7/MainActivity.java
+++ app/src/main/java/mx/com/webmaps/md_ejercicio7/MainActivity.java
@@ -1,12 +1,15 @@
 package mx.com.webmaps.md_ejercicio7;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    FloatingActionButton fab;
     Snackbar snackbar;
 
     @Override
@@ -14,9 +17,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        View view = findViewById(R.id.activy_main);
+        fab = (FloatingActionButton) findViewById(R.id.fab_id);
 
-        snackbar = Snackbar.make(view, "Hello SnackBar", Snackbar.LENGTH_INDEFINITE);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                snackbar = Snackbar.make(v, "Message Sent", Snackbar.LENGTH_INDEFINITE);
+
+                snackbar.setDuration(4000);
+                snackbar.show();
+            }
+        });
+
+
+
+
+        /*snackbar = Snackbar.make(view, "Hello SnackBar", Snackbar.LENGTH_INDEFINITE);
 
         snackbar.setDuration(5000);
 
@@ -25,5 +41,6 @@ public class MainActivity extends AppCompatActivity {
         //snackbar.dismiss();
 
         //Snackbar.make(view,"Hello Snackbar",Snackbar.LENGTH_SHORT).show();
+        */
     }
 }
